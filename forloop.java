fc1bdd743bde91abbdcd4ce3bee930105c8df035
Suyash Sonawane <suyashgit24@gmail.com>
2024-10-21 11:52:19 +0530

    forloop.java
    simple java program using decision making statement for loop

---
+++ forloop.java
@@ -0,0 +1,19 @@
+package dicisionmaking;
+
+public class forloop {
+
+	public static void main(String[] args) {
+			
+			//nested loop
+		for(int hotel=1;hotel<=10;hotel++)
+		{
+			for(int room=1;room<=10;room++)
+			{
+				System.out.println("Diamond");			
+			}
+
+		}
+		
+	}
+
+}
